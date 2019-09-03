2d435b7b9afcc005cd44b4534fcdc42b5655a7e6
adityain98 <adityain98@gmail.com>
2019-09-03 11:18:17 +0700

    perbaikan

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,14 +1,13 @@
 function checkVowel(str) {
-    var data = ['a','i','u','e','o'];
+    var data = ['a','i','u','e','o', ' '];
 
     for(var i = 0; i<str.length; i++){
         var isSame = false;
         for(var j = 0; j<data.length; j++){
             if(str[i] == data[j]){
-                isSame = true;
                 break;
             }
-            if(j == str.length -1 && isSame == false){
+            else if(j == data.length -1 && isSame == false){
                 return false;
             }
         }
@@ -16,4 +15,6 @@ function checkVowel(str) {
     return true;
 }
 
+console.log(checkVowel('andi'));
 console.log(checkVowel('aiueo'));
+console.log(checkVowel('aiueo andi'));
