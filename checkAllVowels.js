8baed102a2e90b937ee82f192d6e69d62768378c
dipadana <putudipadana@gmail.com>
2019-09-03 12:37:03 +0700

    Kumpul checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,27 @@
+function check(str){
+  var data = ['a','i','u','e','o','A','I','U','E','O'];
 
+  var hasil = []
+
+  for(var i = 0; i < str.length; i++){
+    var tanda = false;
+    for(var j = 0; j < data.length; j++){
+      if(str[i] == data[j]){
+        hasil.push(true);
+        tanda = true;
+      }
+    }
+    if(!tanda){
+      hasil.push(false)
+    }
+  }
+  for(var i = 0; i < hasil.length; i++){
+    if(!hasil[i]) return false;
+  }
+  return true;
+}
+
+console.log(check('aiueo'))
+console.log(check('aiAueow'))
+console.log(check('iueouauuuuuOOUOUAIOUAOIOEUOEUOEU'))
+console.log(check('iueouauuuuuOOUOUAIOUAOIOEUOEUOEUg'))
