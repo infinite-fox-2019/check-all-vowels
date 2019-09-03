89becf36515adb2de92917b25a1e0e52591c8f35
Panji SN <panji1595@gmail.com>
2019-09-03 11:06:04 +0700

    Tugas vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkAllVowels (str){
+    str = str.toLowerCase();
+    var strKonsonan = "bcdfghjklmnpqrstvwxyz";
 
+    if(!str){
+        return "Tidak ada kata yang dimasukkan"
+    }
+    for(var i = 0; i < str.length;i++){
+        for (var j = 0; j < strKonsonan.length;j++){
+            if (strKonsonan[j]==str[i]){
+                return false;
+            } 
+        }
+    }
+    return true;
+}
+
+console.log(checkAllVowels("aaaoiueo"));//True
+console.log(checkAllVowels("aaaoijkeo"));//False
+console.log(checkAllVowels("jaaoiueo"));//False
+console.log(checkAllVowels("AaaDSAoiueo"));//False
+console.log(checkAllVowels("aAaoiueo"));//True
+console.log(checkAllVowels(""))//Tidak ada kata yang dimasukkan
