55fc319b71dd45dbcb8093e8fe11950b90acfc2d
akbfitra <akbarfitralistyono@gmail.com>
2019-09-03 13:40:53 +0700

    vowels akbfitra

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,22 @@
 
+
+function cekVokal(string){
+    var str = string.toLowerCase()
+    var temp = ''
+    for(var i = 0; i<str.length;i++){
+      if (str[i] == 'a'|| str[i] == 'i'|| str[i] == 'u' || str[i] == 'e'|| str[i] == 'o'|| str[i] == ' '){ 
+      }
+      else 
+        temp += str[i]
+    }
+    if (temp){
+        return false
+    }
+        return true
+}
+
+console.log(cekVokal('a'))
+console.log(cekVokal('fgh'))
+console.log(cekVokal('aiauaiaoaiiaueoeieu'))
+console.log(cekVokal('aaiuaiuaiuaiuaiuiuiuiauiauss'))
+console.log(cekVokal('aaiuaiuaiuaiuaiuiuiu  iauiau'))
