56bf1aa950f3a6f28091ce3a7ecf7e4ba94e7b4b
evans <karlinevans333@gmail.com>
2019-09-03 10:50:19 +0700

    Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function vokal (str){
+    arr = ["a", "i", "u" , "e", "o"]
+    for(var i = 0 ; i<str.length ; i++){
+        if (arr.indexOf(str[i].toLowerCase()) == -1){
+            return false
+        }
+    }
+    return true
+}
 
+console.log(vokal('aiueo')); //true
+console.log(vokal('AIUEO')); //true
+console.log(vokal('ABICUDEFOG')); //true
+console.log(vokal('abicudefog')); //false
+console.log(vokal('bcdfg')); //false
