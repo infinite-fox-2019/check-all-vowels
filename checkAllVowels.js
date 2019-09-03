4823e5939eaaf4b9c00954a174ea8ef97af680df
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-03 11:39:55 +0700

    checkAllVowels_nadhil

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkVowel(string){
+  var vowel = 'aiueo'
+  var check = false;
+  loop1:
+  for (var i=0; i<string.length;i++){
+    for (var j=0; j<vowel.length;j++){
+      if (string[i]==vowel[j]){
+        check = true;
+        continue loop1;
+      } else {
+        check = false
+      }
+    }
+    if (!check) {
+      return false
+    }
+  }
+  return check
+}
 
+console.log(checkVowel("infinite")) // False
+console.log(checkVowel("ieauo")) // True
+console.log(checkVowel("fox")) // False
