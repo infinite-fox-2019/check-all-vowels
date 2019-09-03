64d68123a242873b5459a4619a5e570e822009b6
adityain98 <adityain98@gmail.com>
2019-09-03 11:08:49 +0700

    selesai

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkVowel(str) {
+    var data = ['a','i','u','e','o'];
 
+    for(var i = 0; i<str.length; i++){
+        var isSame = false;
+        for(var j = 0; j<data.length; j++){
+            if(str[i] == data[j]){
+                isSame = true;
+                break;
+            }
+            if(j == str.length -1 && isSame == false){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+console.log(checkVowel('aiueo'));
