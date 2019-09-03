7e482d4dfe290894a1264c2ba2bf086687bf431d
devitasari <devitas700@gmail.com>
2019-09-03 11:49:11 +0700

    add new file

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowel(str) {
+    str = str.toLowerCase().split('')
+    for (var i=0; i<str.length; i++) {
+        if (!isVocal(str[i])) return false
+    }
+    return true
+}
 
+function isVocal(char) {
+    var refer = ['a','i','u','e','o']
+    if (refer.includes(char)) return true
+    return false
+}
+
+console.log(checkVowel('aaaaAi'))
+console.log(checkVowel('babiBu'))
