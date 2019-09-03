32e5a231d3d8913179e52d26422bb4a7c6ae7bd4
aldinofrizal <aldinofrizal@gmail.com>
2019-09-03 10:50:35 +0700

    finish checkVowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkVowel(inputString){
+    if(!inputString.length)return false
+    var vocal = ['a','i','u','e','o']
+    vocal = vocal.filter(function(eachVocal){
+        return eachVocal === inputString[0]
+    })
+    // console.log(vocal)
+    if(vocal.length)return true
+    else return checkVowel(inputString.slice(1))
+}
 
+console.log(checkVowel('hsss')) //false
+console.log(checkVowel('hsas')) //true
+console.log(checkVowel('ssffsis')) //true
+console.log(checkVowel('xxxxxpppp')) //false
