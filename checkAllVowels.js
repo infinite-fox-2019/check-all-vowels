3008003a0f2ef42c5381816a63a331a843b8296e
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-03 11:13:18 +0700

    add-solution

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+var vowels = {a: 'a', e: 'e', i: 'i', o: 'o', u: 'u'};
 
+function checkAllVowels(str) {
+    if(str === '') {
+        return false;
+    }
+    for(var i = 0; i < str.length; i++) {
+        if(!vowels[str[i]]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(checkAllVowels('aiueoaiueo')) //TRUE
+console.log(checkAllVowels('wyvyrn')) //FALSE
+console.log(checkAllVowels('euibiue')) //FALSE
+console.log(checkAllVowels('ie')) //TRUE
+console.log(checkAllVowels('a')) //TRUE
+console.log(checkAllVowels('')) //FALSE
