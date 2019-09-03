8571b8609377bdaa754f4c5009456d0e2a45c6b5
anggabanny <anggabanny@gmail.com>
2019-09-03 11:32:00 +0700

    update

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
-
+function CheckVowels(data){
+    let pembanding = data.toLowerCase()
+    for(let i = 0 ; i < pembanding.length; i++){
+        switch (pembanding[i]) {
+            case 'a' :{;break;}
+            case 'i' :{;break;}
+            case 'u' :{;break;}
+            case 'e' :{;break;}
+            case 'o' :{;break;}
+            default : return false
+        }
+    }
+    return true
+}
+console.log(CheckVowels('aaaaiiiiioooouu'));
+console.log(CheckVowels('aaiaiaiaoiaoiag'));
