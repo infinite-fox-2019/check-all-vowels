bee6d60140a55a11563936041d4b549f22313045
hoandreas <andreas_ho@hotmail.com>
2019-09-03 11:59:58 +0700

    first commit

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
+function checkAllVowels(str) {
+    var vowels = ['a','i','u','e','o'];
+    for(var i = 0; i < str.length; i++) {
+        if(vowels.indexOf(str[i]) == -1) {
+            return false;
+        }
+    }
+    return true;
+}
 
+console.log(checkAllVowels('aiueo')) // true
+console.log(checkAllVowels('lol')); // false
+console.log(checkAllVowels('aiueob')); // false
+console.log(checkAllVowels('ei')) // true
